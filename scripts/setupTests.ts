509451aa6f48a184be9457b0175a2a9dc35ed573
lorenries <lorenries@users.noreply.github.com>
2020-04-10 11:30:10 -0400

    Add server hashing for isomorphic environments

--- scripts/setupTests.ts
+++ scripts/setupTests.ts
@@ -3,5 +3,8 @@ import { Crypto } from '@peculiar/webcrypto';
 // Polyfills for functionality not available in Node.js
 // TextEncoder
 require('fast-text-encoding');
-// WebCrypto
-(window as any).crypto = new Crypto();
+
+if (typeof window !== 'undefined') {
+  // WebCrypto
+  (window as any).crypto = new Crypto();
+}
